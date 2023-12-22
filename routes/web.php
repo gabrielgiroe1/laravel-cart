c1a79966f0760ddf7cf54b0b7bd405bd174ea755
gabrielgiroe1 <gabriel.giroe@gmail.com>
2023-12-22 19:30:12 +0200

    Update routes to add cart and delete cart

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[PagesController::class, 'index'])->name('home');
+
 Route::get('/shop',[ProductController::class, 'index'])->name('shop');
 Route::get('/shop/{id}',[ProductController::class, 'show'])->name('product');
 
 Route::get('/cart',[CartController::class, 'cart'])->name('cart');
+Route::get('/add-to-cart/{id}',[CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/delete-from-cart/{id}', [CartController::class,'delete'])->name('delete.from.cart');
+
+
 
